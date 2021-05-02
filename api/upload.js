5eda637412c621f8aa14f47f6b020c7390fae0bf
vihangatheturtle <vihanga1432@gmail.com>
2021-05-02 21:14:34 +0100

    Create upload.js

---
+++ api/upload.js
@@ -0,0 +1,9 @@
+module.exports = (req, res) => {
+	console.log("** Uploaded file ", req.file.filename);
+	res.json({'message': 'File uploaded successfully',
+		'result': true,
+		fileName: req.file.filename,
+		path: '/uploads/' +  req.file.filename,
+		url: `http://${host}:${port}/uploads/${req.file.filename}`
+	});
+};
