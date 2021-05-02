63d423eca737ee7b59c19bc575dd800670c9f799
mjk 134 <57556877+mjk134@users.noreply.github.com>
2021-05-02 21:27:39 +0100

    Update upload.js

--- api/upload.js
+++ api/upload.js
@@ -1,14 +1,9 @@
 module.exports = (req, res) => {
-  try {
 	console.log("** Uploaded file ", req.file.filename);
-    res.json({'message': 'File uploaded successfully',
-      'result': true,
-      fileName: req.file.filename,
-      path: '/uploads/' +  req.file.filename,
-      url: `http://${host}:${port}/uploads/${req.file.filename}`
-    });
-  } catch (ex) {
-    console.log(ex.toString());
-    res.send("Failed to upload file");
-  }
+  res.json({'message': 'File uploaded successfully',
+    'result': true,
+    fileName: req.file.filename,
+    path: '/uploads/' +  req.file.filename,
+    url: `http://${host}:${port}/uploads/${req.file.filename}`
+  });
 };
